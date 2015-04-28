711e6665c4170d1b2f157fd21e5493374c074308
Manfred Torres <manfredtorress@gmail.com>
2015-04-28 14:10:09 -0600

    Create eric-sinks-superlative form

---
+++ src/app/forms/eric-sinks-superlative/eric-sinks-superlative.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('uvpBuilderWeb')
+    .run(addMethod);
+
+  addMethod.$inject = [ 'MethodsService' ];
+
+  function addMethod(MethodsService) {
+    var EricSinksSuperlative = function () {
+      this.why = '';
+    };
+
+    EricSinksSuperlative.prototype.template = function () {
+      return this.why;
+    };
+
+    MethodsService.add('EricSinksSuperlative', EricSinksSuperlative);
+  }
+})();

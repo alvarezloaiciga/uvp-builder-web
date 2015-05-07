199857595d6303258b86d5a3356732af86f29ca4
Manfred Torres <manfredtorress@gmail.com>
2015-05-07 13:55:32 -0600

    Remove upload.php

--- src/app/methods/methods.module.js
+++ src/app/methods/methods.module.js
@@ -3,19 +3,5 @@
 
   angular
     .module('uvpBuilderWeb.methods', ['flow', 'ui.router'])
-    .config(flowConfig)
     .value('userBackground', {image: undefined});
-
-  flowConfig.$inject = ['flowFactoryProvider'];
-
-  function flowConfig(flowFactoryProvider) {
-    flowFactoryProvider.defaults = {
-      target: 'app/methods/upload.php',
-      permanentErrors: [404, 500, 501],
-      maxChunkRetries: 1,
-      chunkRetryInterval: 5000,
-      simultaneousUploads: 4,
-      singleFile: true
-    };
-  }
 })();

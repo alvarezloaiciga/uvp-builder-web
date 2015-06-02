6e2e99f615d2e9e461107ab1edffba40ebe88f49
Manfred Torres <manfredtorress@gmail.com>
2015-06-02 16:42:19 -0600

    Add location to directive
    Use location.host instead of location.absUrl

--- src/app/facebook-share-button/facebook-share-button.directive.js
+++ src/app/facebook-share-button/facebook-share-button.directive.js
@@ -14,11 +14,12 @@
     };
   }
 
-  FacebookShareButtonController.$inject = ['$window'];
+  FacebookShareButtonController.$inject = ['$window', '$location'];
 
-  function FacebookShareButtonController($window) {
+  function FacebookShareButtonController($window, $location) {
     var vm = this;
     vm.postUVP = postUVP;
+    var url = $location.host() + '/#/share/';
 
     function postUVP(uvp) {
       FB.ui(
@@ -27,7 +28,7 @@
           action_type: 'uvp-builder:build',
           action_properties: JSON.stringify({
             uvp:{
-              url: 'http://staging.uvp-builder-web.divshot.io/#/share/' + uvp.replace(/ /g,'%20'),
+              url: url + encodeURIComponent(uvp),
               title: 'Watch my project\'s unique value proposition!',
               image: 'http://s29.postimg.org/hgp7dabfb/Screenshot_from_2015_06_02_14_59_37.png'
             }

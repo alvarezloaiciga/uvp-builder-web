81f8b47cbbb266fa2e34c267255545946d10bded
Manfred Torres <manfredtorress@gmail.com>
2015-06-04 11:49:50 -0600

    Add variables for current state and methods, change method definition test, remove 'should from test descriptions'

--- src/app/methods/methods.controller.spec.js
+++ src/app/methods/methods.controller.spec.js
@@ -4,41 +4,46 @@ describe('methods.controller', function(){
 
   var controller, MethodsService, scope, state, compile;
 
+  var CURRENT_STATE = { name: 'someMethod' },
+    CURRENT_METHOD = function(){ this.name = CURRENT_STATE.name },
+    METHODS = {};
+
+  METHODS[CURRENT_STATE.name] = CURRENT_METHOD;
+
   beforeEach(module('uvpBuilderWeb.methods'));
 
-  beforeEach(inject(function(_MethodsService_, $controller, $state, $rootScope, $compile){
+  beforeEach(inject(function(_MethodsService_, $controller, $state, $rootScope){
     MethodsService = _MethodsService_;
-    MethodsService.methods = { someMethod: function(){} };
+    MethodsService.methods = METHODS;
     state = $state;
-    state.current = { name: 'someMethod' };
+    state.current = CURRENT_STATE;
     scope = $rootScope.$new();
     controller = $controller('MethodsController', {
       $scope: scope
     });
-    compile = $compile;
   }));
 
-  it('should define the user backgroun', function() {
+  it('defines the user background', function() {
     expect(scope.userBackground).toBeDefined();
   });
 
-  it('should get the current method\'s name', function(){
-    expect(controller.methodName).toBeDefined();
+  it('gets the current method\'s name', function(){
+    expect(controller.methodName).toEqual(CURRENT_STATE.name);
   });
 
-  it('should define the current method', function () {
-    expect(controller.method).toBeDefined();
+  it('defines the current method', function () {
+    expect(controller.method).toEqual(new CURRENT_METHOD());
   });
 
   describe('display', function() {
-    it('should set the value of showUVP to true', function () {
+    it('sets the value of showUVP to true', function () {
       controller.display();
       expect(controller.showUVP).toBeTruthy();
     });
   });
 
   describe('back', function() {
-    it('should set the value of showUVP to false', function () {
+    it('sets the value of showUVP to false', function () {
       controller.showUVP = true;
       controller.back();
       expect(controller.showUVP).toBeFalsy();

bbc1c5dc4ed2380913ec802b2657e146bddb890e
Manfred Torres <manfredtorress@gmail.com>
2015-06-11 08:32:36 -0600

    Configure karma with gulp

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -3,12 +3,62 @@
 var gulp = require('gulp');
 
 var $ = require('gulp-load-plugins')();
-var karma = require('karma').server;
 
-module.exports = function() {
-  gulp.task('test', ['scripts'], function() {
-    karma.start({
-      configFile: __dirname + '/../karma.conf.js'
+var wiredep = require('wiredep');
+var karma = require('karma');
+var concat = require('concat-stream');
+var _ = require('lodash');
+
+module.exports = function(options) {
+  function listFiles(callback) {
+    var bowerDeps = wiredep({
+      directory: 'bower_components',
+      exclude: [/foundation\.js/, /foundation\.css/],
+      dependencies: true,
+      devDependencies: true
     });
+
+    var specFiles = [
+      options.src + '/**/*.spec.js',
+      options.src + '/**/*.mock.js'
+    ];
+
+    var htmlFiles = [
+      options.src + '/**/*.html'
+    ];
+
+    var srcFiles = [
+      options.src + '/**/*.module.js',
+      options.src + '/**/*.route.js',
+      options.src + '/**/*.service.js',
+      options.src + '/**/*.controller.js',
+      options.src + '/**/*.directive.js'
+    ];
+
+
+    gulp.src(srcFiles)
+      .pipe(concat(function(files) {
+        callback(bowerDeps.js
+          .concat(_.pluck(files, 'path'))
+          .concat(htmlFiles)
+          .concat(specFiles));
+      }));
+  }
+
+  function runTests (singleRun, done) {
+    listFiles(function(files) {
+      karma.server.start({
+        configFile: __dirname + '/../karma.conf.js',
+        files: files,
+        singleRun: singleRun
+      }, done);
+    });
+  }
+
+  gulp.task('test', ['scripts'], function(done) {
+    runTests(true, done);
+  });
+  gulp.task('test:auto', ['watch'], function(done) {
+    runTests(false, done);
   });
 };
--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function(config) {
       'src/**/*.js': ['coverage']
     },
 
-    reporters: ['progress', 'coverage'],
+    reporters: ['coverage', 'progress'],
 
     coverageReporter: {
       reporters:[
@@ -38,30 +38,7 @@ module.exports = function(config) {
           type: 'text'
         }
       ]
-    },
-
-    files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-cookies/angular-cookies.js',
-      'bower_components/angular-touch/angular-touch.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-foundation/mm-foundation.js',
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-ui-router/release/angular-ui-router.js',
-      'bower_components/ng-flow/dist/ng-flow.js',
-      'src/app/index.js',
-      'src/**/*.module.js',
-      'src/**/*.route.js',
-      'src/**/*.service.js',
-      'src/**/*.controller.js',
-      'src/**/*.directive.js',
-      'src/**/*.spec.js'
-    ],
-
-    singleRun: true
+    }
   };
 
   config.set(configuration);

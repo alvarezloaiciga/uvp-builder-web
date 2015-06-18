94b223431bb90201f5f7e4f82f3ef4ea0b82527f
Manfred Torres <manfredtorress@gmail.com>
2015-06-18 09:46:01 -0600

    Add getMethod(int index)

--- src/app/methods/methods.service.js
+++ src/app/methods/methods.service.js
@@ -16,10 +16,15 @@
 
     function getMethod(method) {
       var Method = null;
-      for (var i=0; i<this.methods.length; i++ ) {
-        if (this.methods[i].slug === method){
-          Method = this.methods[i];
+      if (typeof method === 'string') {
+        for (var i = 0; i < this.methods.length; i++) {
+          if (this.methods[i].slug === method) {
+            Method = this.methods[i];
+          }
         }
+      }
+      else if (typeof method === 'number' && this.methods[method]) {
+        Method = this.methods[method];
       }
       if(Method !== null){
         return Method;

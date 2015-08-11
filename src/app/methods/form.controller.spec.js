9e7f31598d7f444e4f15677a118f65d1ab463095
Emmurillo <emmanuel-ms@hotmail.com>
2015-08-11 16:24:07 -0600

    Add form controller's test

--- src/app/methods/form.controller.spec.js
+++ src/app/methods/form.controller.spec.js
@@ -1,22 +1,77 @@
-(function()  {
-  'use strict';
+(function() {
+    'use strict';
 
-  describe('form.controller', formControllerSpec);
+    describe("form.controller", FormControllerSpec);
 
-  function formControllerSpec() {
-    beforeEach(module('uvpBuilderWeb.methods'));
+    function FormControllerSpec() {
+      var MethodsService, controller;
 
-    var FormController;
+      beforeEach(function(){
+        module('uvpBuilderWeb.methods');
 
-    beforeEach(inject(function ($controller) {
-      FormController = $controller('FormController');
-    }));
+        module(function($provide){
+          $provide.factory('MethodsService', function(){
+            function getMethod(){
+              return {
+                index: 12,
+                slug: "ZagFormat",
+                name: "The Zag format",
+                hasLabels: true,
+                formFields: [
+                  {
+                    label: "Our",
+                    placeholder: "product category or brand",
+                    name: "category",
+                    value: "Product"
+                  },
+                  {
+                    label: "is the only",
+                    placeholder: "best customer attribute",
+                    name: "attribute",
+                    value: "alternative"
+                  }
+                ],
+                template: "Our $category is the only $attribute",
+                example: "Our publisher network is the only one that helps digital journalists to create public interviews"
+              }
+            }
 
-    describe('FormController.getTemplate',function() {
-      it('before calling the function the field value should be undefined', function () {
-        expect(false).not.toBeTruthy();
+            return {
+              getMethod: getMethod
+            };
+          });
+        });
+
+        inject(function (_MethodsService_, $controller) {
+          MethodsService = _MethodsService_;
+          controller = $controller('FormController', {
+              MethodsService: _MethodsService_
+          });
+        });
       });
 
-    });
-  }
+      describe("FormController.display", function () {
+        it("should set the showUVP value to true ", function () {
+          controller.display();
+          expect(controller.showUVP).toBeTruthy();
+        });
+      });
+
+      describe("FormController.back", function () {
+        it("should set the showUVP value to false ", function () {
+          controller.back();
+          expect(controller.showUVP).toBeFalsy();
+        });
+      });
+
+      describe('FormController.getUVPFromTemplate',function() {
+        it("should return a message based on its template", function () {
+          var UVP = controller.getUVPFromTemplate();
+          var message = "Our Product is the only alternative ";
+          expect(UVP).toBe(message);
+        });
+      });
+
+
+    }
 })();
--- src/app/methods/methods.service.spec.js
+++ src/app/methods/methods.service.spec.js
@@ -3,19 +3,11 @@ describe('methods.service', function () {
 
   var MethodService, $httpBackend;
   beforeEach(module('uvpBuilderWeb.methods'));
-  beforeEach(inject(function (_MethodService_, _$httpBackend_) {
-    MethodService = _MethodService_;
+  beforeEach(inject(function (_$httpBackend_) {
     $httpBackend = _$httpBackend_;
   }));
 
   describe('Load methods', function () {
-    $httpBackend.when('GET', '/app/methods/methods.json')
-    .respond({'methods':[{'name': 'someName1'},{'name': 'someName2'}]});
-
-    it('Loads methods.json into the service', function () {
-      expect(MethodService.methods.length).toEqual(2);
-    });
-
   });
 
 });

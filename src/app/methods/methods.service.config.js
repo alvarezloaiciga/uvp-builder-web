cd2e20476ba69e7d225cfb9d96c325bf9564de99
Emmurillo <emmanuel-ms@hotmail.com>
2015-08-24 10:45:05 -0600

    Retrieve methods from different files based on selected Language

--- src/app/methods/methods.service.config.js
+++ src/app/methods/methods.service.config.js
@@ -5,16 +5,10 @@
     .module('uvpBuilderWeb.methods')
     .run(setMethods);
 
-  setMethods.$inject = ['MethodsService', '$http', 'LanguageService'];
+  setMethods.$inject = ['MethodsService'];
 
-  function setMethods(MethodsService, $http, LanguageService) {
-    $http.get('/app/methods/methods.json')
-      .success(function(data) {
-        MethodsService.set(data.methods);
-      }).
-      error(function(data) {
-        throw new Error( data || 'Request failed');
-      });
+  function setMethods(MethodsService) {
+    MethodsService.setLanguage('EN');
   }
 
 })();

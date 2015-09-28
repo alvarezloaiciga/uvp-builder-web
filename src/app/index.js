febc0c74d2259ee4527ebed94bbf8c19f1cddc17
Emmurillo <emmanuel-ms@hotmail.com>
2015-09-28 15:14:08 -0600

    Use environment to dinamically set API's URL

--- src/app/index.js
+++ src/app/index.js
@@ -14,7 +14,6 @@
     'uvpBuilderWeb.language',
     'uvpBuilderWeb.methods',
     'uvpBuilderWeb.home'])
-    .constant('API_URL', 'http://localhost:8000')
     .value('localeSupported', [
       'en-US',
       'es-CR'
